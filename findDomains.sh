f2b2d9418f29389140092a8b8af83d5ff2af38f3
Dylan <savetrees@ymail.com>
2018-04-24 23:15:08 -0500

    Delete findDomains.sh

--- findDomains.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for f in ./*fasta ;
-do
-name="$(echo '${f}' | cut -d '.' -f 1") ; hold file name without extension 
-mkdir SMART"$name" ;
-mkdir SMART"$name"_keep ;
-perl SMART_batch.pl --inputFile "${f}" --outputDirectory SMART"$name" ; 
-find SMART"$name" -name "*.txt" | xargs egrep "SANT" | cut -d ":" -f 1 | xargs -I  '{}' mv {} SMART"$name"_keep ; done
